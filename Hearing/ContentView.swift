783d05bb986bd27a9d692fb3e9268ba4e078ef99
半熟木屋 <164508906@qq.com>
2020-05-24 09:09:30 +0800

    Initial Commit

---
+++ Hearing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hearing
+//
+//  Created by 李招雄 on 2020/5/24.
+//  Copyright © 2020 李招雄. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
